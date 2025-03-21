0eb15f181f96f30e0293f98108fc73716ef98b35
Sai Prabhas <bandasaiprabhas@gmail.com>
2025-03-21 10:43:03 +0000

    feat: connect Node.js backend to MongoDB Compass

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors")
+const connectDB = require('./config/db');
 
 dotenv.config(); // load env variables 
 
@@ -11,6 +12,7 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
+connectDB();
 app.get("/",(req,res)=>{
     res.send('API is running..!');
 })
